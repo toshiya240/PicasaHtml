6e094475380034773b3246ce59cece3871ee9c8e
Toshiya NISHIO <toshiya240@gmail.com>
2012-08-12 07:41:40 +0900

    # Version 2.6.0 - フォーマットをプリセットから選択またはユーザ定義可能に変更 - 通信など何かしらのアクションが起きるもの以外はテーマを c(白いもの)に変更

--- PicasaHtml.js
+++ PicasaHtml.js
@@ -1,6 +1,6 @@
 /*
  * PicasaHtml.js
- * @version 2.5.0
+ * @version 2.6.0
  * @author Toshiya NISHIO(http://www.toshiya240.com)
  */
 function detectEnv() {
@@ -21,280 +21,311 @@ function detectEnv() {
 }
 
 var cookie = {
-    set: function(key, obj) {
-        var expires = new Date(2030, 1, 1);
-        document.cookie = key + "=" + escape(obj) + ";expires=" + expires.toGMTString();
-    },
-    get: function(key) {
-        var cookies = document.cookie.split("; ");
-        for (var i = 0; i < cookies.length; ++i) {
-            var pair = cookies[i].split("=");
-            if (pair[0] == key) {
-                return unescape(pair[1]);
-            }
-        }
-        return "";
-    },
-    remove: function(key) {
-        var expires = new Date(1900, 1, 1);
-        document.cookie = key + "='';expires=" + expires.toGMTString();
-    },
-    clear: function() {
-        var cookies = document.cookie.split("; ");
-        for (var i = 0; i < cookies.length; ++i) {
-            var pair = cookies[i].split("=");
-            this.remove(pair[0]);
-        }
+  set: function(key, obj) {
+    var expires = new Date(2030, 1, 1);
+    document.cookie = key + "=" + escape(obj) + ";expires=" + expires.toGMTString();
+  },
+  get: function(key) {
+    var cookies = document.cookie.split("; ");
+    for (var i = 0; i < cookies.length; ++i) {
+      var pair = cookies[i].split("=");
+      if (pair[0] == key) {
+        return unescape(pair[1]);
+      }
     }
+    return "";
+  },
+  remove: function(key) {
+    var expires = new Date(1900, 1, 1);
+    document.cookie = key + "='';expires=" + expires.toGMTString();
+  },
+  clear: function() {
+    var cookies = document.cookie.split("; ");
+    for (var i = 0; i < cookies.length; ++i) {
+      var pair = cookies[i].split("=");
+      this.remove(pair[0]);
+    }
+  }
 };
-
 var storage = cookie;
 
-var loadConfig = function() {
-    var confUserID = storage.get("conf_userID");
-    var confTmp = storage.get("conf_tmp2");
-    if (confTmp == "") confTmp = "http://dl.dropbox.com/u/529339/bookmarklet/PicasaTemplate2.csv";
-    var confFmt = storage.get("conf_fmt");
-    if (confFmt == "") confFmt = 1;
+function loadConfig() {
+  var confUserID = storage.get("conf_userID");
+  $("#conf-userID").val(confUserID);
 
-    $("#conf-userID").val(confUserID);
-    $("#conf-tmp").val(confTmp);
-    $("#conf-fmt").val(confFmt);
+  var selectedIndex = storage.get("conf_picasahtml_fmt_index");
+  if (!selectedIndex) selectedIndex = 0;
+  var $presetSelect = $("#preset");
+  $presetSelect.get(0).selectedIndex = selectedIndex;
+  presetSelectionChanged();
 };
 
-var saveConfig = function() {
-    if ($("#conf-userID").val() == "") {
-        showError("ユーザID を入力してください。");
-        return false;
-    }
-    storage.set("conf_userID", $("#conf-userID").val());
-    storage.set("conf_tmp2", $("#conf-tmp").val());
-    storage.set("conf_fmt", $("#conf-fmt").val());
+function saveConfig() {
+  if ($("#conf-userID").val() == "") {
+    showMsg("ユーザID を入力してください。");
+    return false;
+  }
+  storage.set("conf_userID", $("#conf-userID").val());
+  storage.set("conf_picasahtml_fmt_index", $("#preset").get(0).selectedIndex);
+  var $selectedPreset = $("#preset option:selected");
+  if ($selectedPreset.text() == "カスタム") {
+    storage.set("conf_picasahtml_fmt_custom", $("#format").val());
+  }
 
-    return true;
+  return true;
 };
 
-var clearConfig = function() {
-    //storage.clear();
-    storage.set("conf_userID", "");
-    storage.set("conf_tmp2", ""); 
-    storage.set("conf_fmt", "");
-    loadConfig();
-    showError("データベースをクリアしました。");
+function clearConfig() {
+  //storage.clear();
+  storage.set("conf_userID", "");
+  storage.set("conf_picasahtml_fmt_index", "0");
+  loadConfig();
+  showMsg("データベースをクリアしました。");
 };
 
-var insertToMoblogger = function() {
-    $.mobile.changePage("#main");
-    var text = $("#ta").val();
-    var url = "moblogger://append?text=" + encodeURIComponent(text);
-    window.location = url;
+var preset = {
+  '写真のみ(インライン)':"<img class='picasa_photo' src='${imgURL}' alt='${title}' width='${width}' height='${height}' />",
+  '写真のみ(ブロック)':"<p><img class='picasa_photo' src='${imgURL}' alt='${title}' width='${width}' height='${height}' /></p>",
+  'cite付き(ブロック)':"<p><img class='picasa_photo' src='${imgURL}' alt='${title}' width='${width}' height='${height}' /><br /><cite>${title} Photo by ${nickname}</cite></p>",
+  '説明付き(ブロック)':"<p>${description}<br /><img class='picasa_photo' src='${imgURL}' alt='${title}' width='${width}' height='${height}' /></p>",
+  'カスタム':""
+};
+var placeholder = {
+  'Picasa 上のニックネーム':'${nickname}',
+  'タイトル':'${title}',
+  '画像のURL':'${imgURL}',
+  '画層の幅':'${width}',
+  '画像の高さ':'${height}',
+  '画像の説明':'${description}',
+};
+function presetSelectionChanged() {
+  var $selectedPreset = $("#preset option:selected");
+  $("#format").val($selectedPreset.val());
+
+  if ($selectedPreset.text() == 'カスタム') {
+    $("#placeholder-container").show();
+    $("#copy-preset-container").hide();
+    $("#format").attr("readonly", false);
+  } else {
+    $("#placeholder-container").hide();
+    $("#copy-preset-container").show();
+    $("#format").attr("readonly", true);
+  }
+}
+function insertPlaceholder(label) {
+  var ph = placeholder[label];
+  var format = $("#format").get(0);
+  var orig = format.value;
+  var pos = format.selectionStart;
+  var npos = pos + ph.length;
+  format.value = orig.substr(0, pos) + ph + orig.substr(pos);
+  format.setSelectionRange(npos, npos);
+  format.focus();
+}
+$("#main").live("pagebeforecreate", function() {
+  var $placeholderSelect = $("#placeholder");
+  for (var label in placeholder) {
+    $placeholderSelect.append($("<a data-role='button'>").text(label).attr('href', 'javascript:insertPlaceholder("'+label+'");'));
+  }
+
+  preset['カスタム'] = storage.get("conf_picasahtml_fmt_custom");
+  var $presetSelect = $("#preset");
+  for (var label in preset) {
+    $presetSelect.append($("<option>").text(label).val(preset[label]));
+  }
+  $presetSelect.change(presetSelectionChanged);
+});
+
+function copyPresetToCustom() {
+  var $selectedPreset = $("#preset option:selected");
+  var $custom = $('#preset option:contains("カスタム")');
+  $custom.val($selectedPreset.val());
+  showMsg("現在選択しているプリセットの内容を「カスタム」にコピーしました。");
 }
 
-var launchMobloggerAndCopy = function() {
-    $.mobile.changePage("#main");
-    var text = $("#ta").val();
-    var url = "moblogger://pboard?text=" + encodeURIComponent(text);
-    window.location = url;
+function insertToMoblogger() {
+  $.mobile.changePage("#main");
+  var text = $("#ta").val();
+  var url = "moblogger://append?text=" + encodeURIComponent(text);
+  window.location = url;
 }
 
-var insertToDraftpad = function() {
-    insertToDp($("#ta").val());
+function launchMobloggerAndCopy() {
+  $.mobile.changePage("#main");
+  var text = $("#ta").val();
+  var url = "moblogger://pboard?text=" + encodeURIComponent(text);
+  window.location = url;
+}
+
+function insertToDraftpad() {
+  insertToDp($("#ta").val());
 };
 
-var myProcess = function(objectFromDraftPad) {
-    var originalText = '';
-    if (objectFromDraftPad && objectFromDraftPad.text ) {
-        originalText = objectFromDraftPad.text;
-    };
-    window.insertToDp = function(resultHtml) {
-        var insertingText = originalText + "\n" + resultHtml;
-        draftpad.replace(insertingText, insertingText.length, 0);
-    };
+function myProcess(objectFromDraftPad) {
+  var originalText = '';
+  if (objectFromDraftPad && objectFromDraftPad.text ) {
+    originalText = objectFromDraftPad.text;
+  };
+  window.insertToDp = function(resultHtml) {
+    var insertingText = originalText + "\n" + resultHtml;
+    draftpad.replace(insertingText, insertingText.length, 0);
+  };
 };
 
-function showError(msg) {
-    $("#error-msg").text(msg);
-    $("<a href='#error-page' data-rel='dialog'></a>").click().remove();
+function showMsg(msg) {
+  $("#error-msg").text(msg);
+  $("<a href='#error-page' data-rel='dialog'></a>").click().remove();
 }
 
 $(function() {
-    var env = detectEnv();
-    if (env == "iOS in-app") {
-        window.location.href = "draftpad:///webdelegate?load=myProcess";
-        $("#source-containter").hide();
-    } else {
-        $("#dpbutton").hide();
-    }
-    if (env != "Mobile Safari") {
-        $("#mb-buttons").hide();
-    }
-    loadConfig();
-    var userID = $("#conf-userID").val();
-    if (userID == "") {
-        $.mobile.changePage("#conf", {transition:"flip"});
-    } else {
-        getAlbums();
-    }
+  var env = detectEnv();
+  if (env == "iOS in-app") {
+    window.location.href = "draftpad:///webdelegate?load=myProcess";
+    $("#source-containter").hide();
+  } else {
+    $("#dpbutton").hide();
+  }
+  if (env != "Mobile Safari") {
+    $("#mb-buttons").hide();
+  }
+  loadConfig();
+  var userID = $("#conf-userID").val();
+  if (userID == "") {
+    $.mobile.changePage("#conf", {transition:"flip"});
+  } else {
+    getAlbums();
+  }
 });
 
 
 var picasaUrlBase = "http://picasaweb.google.com/data/feed/api/user/"
 
 function getAlbums() {
-    var userID = $("#conf-userID").val();
-    var url = picasaUrlBase + userID + "?kind=album&alt=json"
-        $.mobile.showPageLoadingMsg();
-    $.ajax({
-        url: url,
-        async: true,
-        cache: false,
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            var items = data.feed.entry;
-            if (items.length) {
-                $("#album").empty();
-                $("<option>").val("").text("All Albums").appendTo("#album");
-                for (var i = 0; i < items.length; i++) {
-                    title = items[i].title.$t;
-                    albumid = items[i].gphoto$id.$t;
-                    $("<option>").val(albumid).text(title).appendTo("#album");
-                }
-                $("#album").selectedIndex = 0;
-                $("#album").selectmenu("refresh");
-            }
-            $.mobile.hidePageLoadingMsg();
-            if (!items.length) {
-                showError("No Albums.");
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            $.mobile.hidePageLoadingMsg();
-            showError(textStatus + ":" + errorThrown + "(1)");
+  var userID = $("#conf-userID").val();
+  var url = picasaUrlBase + userID + "?kind=album&alt=json";
+  $.mobile.showPageLoadingMsg();
+  $.ajax({
+    url: url,
+    async: true,
+    cache: false,
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+      var items = data.feed.entry;
+      if (items.length) {
+        $("#album").empty();
+        $("<option>").val("").text("All Albums").appendTo("#album");
+        for (var i = 0; i < items.length; i++) {
+          title = items[i].title.$t;
+          albumid = items[i].gphoto$id.$t;
+          $("<option>").val(albumid).text(title).appendTo("#album");
         }
-    });
+        $("#album").get().selectedIndex = 0;
+        $("#album").selectmenu("refresh");
+      }
+      $.mobile.hidePageLoadingMsg();
+      if (!items.length) {
+        showMsg("No Albums.");
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $.mobile.hidePageLoadingMsg();
+      showMsg(textStatus + ":" + errorThrown + "(1)");
+    }
+  });
 }
 
 function getPhotos() {
-    $.mobile.showPageLoadingMsg();
+  $.mobile.showPageLoadingMsg();
 
-    var url = $("#conf-tmp").val();
-    var fmt = $("#conf-fmt").val();
-    var descformat = "";
-    $.ajax({
-        url: url,
-        async: true,
-        cache: false,
-        type: "GET",
-        dataType: "text",
-        success: function(data) {
-            var lines = data.split("\n");
-            var index = parseInt(fmt) + 8;
-            if (lines.length <= index) {
-                index = 9;
-            }
-            descformat = lines[index];
-            if (descformat == "") {
-                $.mobile.hidePageLoadingMsg();
-                return;
-            }
-            getPhotos2(descformat);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            $.mobile.hidePageLoadingMsg();
-            showError(textStatus + ":" + errorThrown + "(2)");
+  var descformat = $("#format").val();
+  var selectedAlbum = $("#album option:selected").val();
+  var url = picasaUrlBase + $("#conf-userID").val();
+  if (selectedAlbum != "") {
+    url = url + "/albumid/" + selectedAlbum;
+  }
+  var maxResults = $("#max").val();
+  if (maxResults == "") {
+    maxResults = "20";
+  }
+  var imgmax = $("#imgmax").val();
+  url = url + "?kind=photo&alt=json&access=public&imgmax=" + imgmax
+    + "&max-results=" + (selectedAlbum == "" ? maxResults : "999");
+  var tag = $("#tag").val();
+  if (tag != "") {
+    url = url + "&tag=" + encodeURIComponent(tag);
+  }
+  $.ajax({
+    url: url,
+    async: true,
+    cache: false,
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+      var items = data.feed.entry;
+      if (items && items.length) {
+        var x = "";
+        if (selectedAlbum == "") {
+          items = items.reverse();
         }
-    });
-}
-
-function getPhotos2(descformat) {
-    var selectedAlbum = $("#album option:selected").val();
-    var url = picasaUrlBase + $("#conf-userID").val();
-    if (selectedAlbum != "") {
-        url = url + "/albumid/" + selectedAlbum;
-    }
-    var maxResults = $("#max").val();
-    if (maxResults == "") {
-        maxResults = "20";
-    }
-    var imgmax = $("#imgmax").val();
-    url = url + "?kind=photo&alt=json&access=public&imgmax=" + imgmax
-        + "&max-results=" + (selectedAlbum == "" ? maxResults : "999");
-    var tag = $("#tag").val();
-    if (tag != "") {
-        url = url + "&tag=" + encodeURIComponent(tag);
-    }
-    $.ajax({
-        url: url,
-        async: true,
-        cache: false,
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            var items = data.feed.entry;
-            if (items && items.length) {
-                var x = "";
-                if (selectedAlbum == "") {
-                    items = items.reverse();
-                }
-                for (var i = 0; i < items.length; i++) {
-                    var title = items[i].media$group.media$title.$t;
-                    var content = items[i].media$group.media$content[0];
-                    var imgURL = content.url;
-                    var width = content.width;
-                    var height = content.height;
-                    var nickname = items[i].media$group.media$credit[0].$t;
-                    var description = items[i].media$group.media$description.$t;
-                    var y = String(descformat);
-                    y = y.replace(/\${title}/g, title);
-                    y = y.replace(/\${imgURL}/g, imgURL);
-                    y = y.replace(/\${width}/g, width);
-                    y = y.replace(/\${height}/g, height);
-                    y = y.replace(/\${nickname}/g, nickname);
-                    y = y.replace(/\${description}/g, description);
-                    x = x + y + "\n\n";
-                }
-                var first = $("#ta").val().length == 0;
-                $("#ta").val(x);
-                var preview = $("#preview");
-                preview.html(x);
-                $.each(preview.children(), function(i) {
-                    var li = $("<li>");
-                    var link = $("<a>").attr("href", "javascript:previewImg("+i+");");
-                    /*
-                     * サムネイル表示時のレンダリングがうまくいかないので
-                     * 自前でクラスを設定する。
-                     */
-                    li.addClass("ui-li-has-thumb");
-                    $(this).find("img").addClass("ui-li-thumb");
-
-                    link.append($(this));
-                    li.append(link);
-                    preview.append(li);
-                });
-                if (items && items.length && !first) {
-                    preview.listview('refresh');
-                }
-            }
-            $.mobile.hidePageLoadingMsg();
-            if (!items || !items.length) {
-                showError("No Photos.");
-                return;
-            }
-            $.mobile.changePage("#result", {transition:"pop"});
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            $.mobile.hidePageLoadingMsg();
-            showError(textStatus + ":" + errorThrown + "(3)");
+        for (var i = 0; i < items.length; i++) {
+          var title = items[i].media$group.media$title.$t;
+          var content = items[i].media$group.media$content[0];
+          var imgURL = content.url;
+          var width = content.width;
+          var height = content.height;
+          var nickname = items[i].media$group.media$credit[0].$t;
+          var description = items[i].media$group.media$description.$t;
+          var y = String(descformat);
+          y = y.replace(/\${title}/g, title);
+          y = y.replace(/\${imgURL}/g, imgURL);
+          y = y.replace(/\${width}/g, width);
+          y = y.replace(/\${height}/g, height);
+          y = y.replace(/\${nickname}/g, nickname);
+          y = y.replace(/\${description}/g, description);
+          x = x + y + "\n\n";
         }
-    });
+        var first = $("#ta").val().length == 0;
+        $("#ta").val(x);
+        var preview = $("#preview");
+        preview.html(x);
+        $.each(preview.children(), function(i) {
+          var li = $("<li>");
+          var link = $("<a>").attr("href", "javascript:previewImg("+i+");");
+          /*
+           * サムネイル表示時のレンダリングがうまくいかないので
+           * 自前でクラスを設定する。
+           */
+          li.addClass("ui-li-has-thumb");
+          $(this).find("img").addClass("ui-li-thumb");
+
+          link.append($(this));
+          li.append(link);
+          preview.append(li);
+        });
+        if (items && items.length && !first) {
+          preview.listview('refresh');
+        }
+      }
+      $.mobile.hidePageLoadingMsg();
+      if (!items || !items.length) {
+        showMsg("No Photos.");
+        return;
+      }
+      $.mobile.changePage("#result", {transition:"pop"});
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $.mobile.hidePageLoadingMsg();
+      showMsg(textStatus + ":" + errorThrown + "(3)");
+    }
+  });
 }
 
 function previewImg(index) {
-    $.mobile.changePage("#preview-page", {transition:"slide"});
-    var imgObject = $("#preview").children().eq(index).find("img").clone();
-    imgObject.removeClass("ui-li-thumb");
-    imgObject.removeAttr("height");
-    imgObject.width("100%");
-    $("#img-preview").html(imgObject);
+  $.mobile.changePage("#preview-page", {transition:"slide"});
+  var imgObject = $("#preview").children().eq(index).find("img").clone();
+  imgObject.removeClass("ui-li-thumb");
+  imgObject.removeAttr("height");
+  imgObject.width("100%");
+  $("#img-preview").html(imgObject);
 }
